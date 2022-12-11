67cd7b7527529668d31e07746d5194092bddfc29
Sultan Kuliev <s.kuliev@fenix-bg.ru>
2022-12-12 03:39:23 +0400

    оптимизация

--- view/staff_view.py
+++ view/staff_view.py
@@ -1,15 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 import tkinter.font as font
-
 from dto.staff_dto import StaffDto
 
 
 class StaffView:
     def __init__(self):
         self.init_heading()
-        self.search_name = self.init_search_name()
         self.search_surname = self.init_search_surname()
+        self.search_name = self.init_search_name()
         self.init_search_button()
         self.init_cancel_button()
         self.tree = self.init_table()
@@ -62,7 +61,6 @@ class StaffView:
         staff_dto = StaffDto()
         searched = staff_dto.select_staff_by_name_surname(surname, name)
         self.set_data_to_table(searched)
-
 
     def init_search_button(self):
         btn = ttk.Button(text="Поиск", command=self.search)
